b0e03c5915cc81862ba792ba535e4ad8b3ae5cb2
Maistelujaska <jaakko.j.rautio@gmail.com>
2025-07-22 14:11:38 +0300

    trying to fix camera controls

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -3,15 +3,15 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     public Transform cameraPosition;
-    /*public GameObject player;
+    public GameObject player;
     private Vector3 offset;
-     Start is called once before the first execution of Update after the MonoBehaviour is created
+    
     void Start()
     {
         offset = transform.position - player.transform.position;
     }
-    */
-    // Update is called once per frame
+    
+    
     void Update()
     {
         transform.position = cameraPosition.position;
